441eb53434a853874c9818caa082a126601f5d7d
onostar <onostarkels@gmail.com>
2022-11-09 15:05:25 +0100

    updatd

---
+++ header.php
@@ -0,0 +1,59 @@
+<section class="top_head" id="topHeader">
+            <div class="social_media">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            <div class="contact_phone">
+                <ul>
+                    <li>
+                        <a><i class="far fa-clock"></i>Mon - Fri : 8:00am - 5:00pm</a>
+                    </li>
+                    <li><a href="javascript:void(0)"><i class="far fa-envelope"></i>info@iginedioncollegeofhealth.com</a></li>
+                   
+                    <li><a href="javascript:void(0);"><i class="fas fa-users"></i>Alumni</a></li>
+                    <li><a href="javascript:void(0)">Portal<i class="fas fa-sign-in-alt"></i></a></li>
+                </ul>
+            </div>
+        </section>
+    <section id="aboutBanner">
+        <header id="mainHeader" class="main_header">
+            <h1>
+                <a href="index.php">
+                    <img src="images/logo.png" alt="logo">
+                </a>
+            </h1>
+            <nav id="navigation">
+                <ul>
+                    <li><a href="about.php" title="who we are"><i class="fas fa-building-columns"></i>Who we are</a></li>
+                    <li>
+                        <a href="javascript:void(0);" title="Courses we offer"><i class="fa-solid fa-book"></i>Courses <i class="fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="community_health.php"><i class="fa-solid fa-house-medical"></i>Community health</a></li>
+                            <li><a href="pharm_tech.php"><i class="fas fa-user-doctor"></i> Pharmacy technician</a></li>
+                            <li><a href="fundamental_health.php"><i class="fas fa-bed-pulse"></i> Fundamental health technology</a></li>
+                            <li><a href="lab_tech.php"><i class="fas fa-heart-pulse"></i> Med Lab technology</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);" title="What we do"><i class="fas fa-briefcase"></i>Other Services <i class="fas fa-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="petroleum.html"><i class="fa-solid fa-gifts"></i> Event centre</a></li>
+                            <li><a href="waste_management.html"><i class="fas fa-paper-plane"></i> Skill Acquisition</a></li>
+                            
+                            
+                        </ul>
+                    </li>
+                    <li><a href="#investment" title="News and updates"><i class="fa-solid fa-user-graduate"></i>Events</a></li>
+                    <li><a href="admissions.php" title="Apply for admission"><i class="fa-solid fa-graduation-cap"></i>Admissions</a></li>
+                    <!-- <li><a href="recruitment.html" title="Job recruitment">Career</a></li> -->
+                    <!-- <li><a href="blog.html" title="Job recruitment">Our Blog</a></li> -->
+                    <li id="login"><a href="contact.html" title="Contact us">Contact us<i class="fas fa-headset"></i></a></li>
+                </ul>
+            </nav>
+            <div class="menu-icon" onclick="displayMenu()"><a href="javascript:void(0);"><i class="fas fa-bars"></i></a></div>
+        </header>
